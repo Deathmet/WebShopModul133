6b66bd85c1421056db552403352557b5e3dc059e
Deathmet <33684487+Deathmet@users.noreply.github.com>
2018-12-20 13:36:58 +0100

    CSV.Classes
    CSV. Classes added

---
+++ WebShopModul 133/WebApplication1/Persistency/ProductsPersistency.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dorflaedali.Persistency
+{
+    public class ProductsPersistency
+    {
+        private readonly FileContentReader fileContentReader;
+        private readonly FileContentToItemsTransformer toItemsTransformer;
+        private readonly LineToProductTransformer toProductsTransformer;
+
+        public ProductsPersistency()
+        {
+            this.fileContentReader = new FileContentReader();
+            this.toItemsTransformer = new FileContentToItemsTransformer();
+            this.toProductsTransformer = new LineToProductTransformer();
+        }
+
+        public IReadOnlyCollection<Product> GetAll()
+        {
+            var fileContent = this.fileContentReader.ReadFromAppData("products.csv");
+            var items = this.toItemsTransformer.Transform(fileContent);
+            var products = this.toProductsTransformer.Transform(items);
+
+            return products.ToArray();
+        }
+
+        public Product GetById(string productId)
+        {
+            return this.GetAll()
+                .Single(p => p.Id == productId);
+        }
+    }
+}
